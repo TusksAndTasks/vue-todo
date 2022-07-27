5581264b7ef3656708968d51d5c06bb2a40fe814
A.kLYKOV <a.klykov@worksolutions.ru>
2022-07-27 17:58:03 +0300

    feat: add history feature in create-form

--- src/components/NotePageForm.vue
+++ src/components/NotePageForm.vue
@@ -2,7 +2,11 @@
   <form @submit.prevent="addNote">
     <input type="text" v-model="title" @keydown.enter.prevent />
     <ul>
-      <li v-for="todo in todos" :key="todo.key" :class="{ done: todo.isDone }">
+      <li
+        v-for="todo in history[currentStep].todos"
+        :key="todo.key"
+        :class="{ done: todo.isDone }"
+      >
         {{ todo.task }}
       </li>
       <!--TODO: Вот эти листы переделать под отдельный компонент, который может переключаться на инпут-->
@@ -11,6 +15,8 @@
     <input type="checkbox" v-model="inputStatus" @keydown.enter.prevent />
     <input type="submit" content="Submit" />
   </form>
+  <button @click="goBackInHistory">Back</button>
+  <button @click="goForwardInHistory">Forward</button>
 </template>
 
 <script setup lang="ts">
@@ -18,33 +24,65 @@ import { INote, ITodo } from "@/types/interfaces";
 import { ref } from "vue";
 import { notesState } from "@/states/NotesState";
 
-const props = defineProps<{ noteData?: INote }>();
+const props = defineProps<{ noteData?: INote; currentStep: number }>();
+const emits = defineEmits(["increase", "decrease", "setToNull"]);
 
 const title = ref(props.noteData?.title ? props.noteData?.title : "");
-const todos = ref(
-  props.noteData?.todos ? props.noteData?.todos : ([] as Array<ITodo>)
-);
 const inputValue = ref("");
 const inputStatus = ref(false);
 
+const history = ref(
+  props.noteData
+    ? [props.noteData]
+    : ([{ title: "", key: "temporal", todos: [] }] as Array<INote>)
+);
+
+function goForwardInHistory() {
+  if (props.currentStep + 1 < history.value.length) {
+    emits("increase");
+  }
+}
+
+function goBackInHistory() {
+  if (props.currentStep - 1 > -1) {
+    emits("decrease");
+  }
+}
+
 function addTodo() {
-  todos.value.push({
-    task: inputValue.value,
-    key: `todo-${Date.now()}`,
-    isDone: inputStatus.value,
-  });
+  handleCorrectHistoryUpdate();
+  emits("increase");
   inputValue.value = "";
   inputStatus.value = false;
+}
+
+function handleCorrectHistoryUpdate() {
+  if (props.currentStep < history.value.length - 1) {
+    history.value = history.value.slice(0, props.currentStep + 1);
+  }
+  history.value.push({
+    title: title.value,
+    key: "temporal",
+    todos: [
+      ...(history.value[props.currentStep].todos as ITodo[]),
+      {
+        task: inputValue.value,
+        key: `task-${Date.now()}`,
+        isDone: inputStatus.value,
+      },
+    ],
+  });
 }
 
 function addNote() {
   notesState.push({
     title: title.value,
     key: `note-${Date.now()}`,
-    todos: todos.value,
+    todos: history.value[props.currentStep].todos,
   });
   title.value = props.noteData?.title ? title.value : "";
-  todos.value = props.noteData?.todos ? todos.value : [];
+  history.value = [{ title: "", key: "temporal", todos: [] }] as Array<INote>;
+  emits("setToNull");
 }
 </script>
 
--- src/components/NotesPage.vue
+++ src/components/NotesPage.vue
@@ -1,11 +1,18 @@
 <template>
   <NotesPageNote v-for="note in notesState" :noteData="note" :key="note.key" />
-  <NotePageForm />
+  <NotePageForm
+    :currentStep="currentStep"
+    @increase="++currentStep"
+    @decrease="--currentStep"
+    @setToNull="currentStep = 0"
+  />
 </template>
 
 <script setup lang="ts">
 import { notesState } from "@/states/NotesState";
-
+import { ref } from "vue";
 import NotesPageNote from "@/components/NotesPageNote.vue";
 import NotePageForm from "@/components/NotePageForm.vue";
+
+const currentStep = ref(0);
 </script>
